2d747af65239416b56c28a0fb35ff0fce6e6de31
jc3213 <jc3213@users.noreply.github.com>
2017-04-10 16:00:36 +0800

    Create FixedBlockingForYoukuAntiAds.ss.js

---
+++ SS.js/FixedBlockingForYoukuAntiAds.ss.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.version = "1.0";  //not functional
+exports.update = "https://raw.githubusercontent.com/jc3213/Misc/master/SS.js/SimpleProxy.ss";  //not functional
+exports.on = youkuStyleSheet;
+
+var PageMod = require("sdk/page-mod");
+
+function youkuStyleSheet() {
+  PageMod.PageMod({
+    include: "*.youku.com",
+    contentStyle: ""
+    + ".danmuoff .vpactionv5_iframe_wrap {"
+    +   "top: auto !important;"
+    + "}"
+    + ""
+    + ".play_area{"
+    +   "margin-bottom: 70px !important;"
+    + "}"
+  });
+}
